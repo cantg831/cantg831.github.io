e32da3ff9ecfa38e889ed06f90b2bebdf9326174
Gabrielle Cantamessa <cantg831@newschool.edu>
2017-03-01 21:27:59 -0500

    edits to balloons

--- vars_cond_loops/js/script.js
+++ vars_cond_loops/js/script.js
@@ -1,25 +1,45 @@
 $(document).ready(function() {
 
+	
+	var numOfBalloons = 50;
 	var popCount = 0;
 
-	$(".balloon").on("click",function() {
+
+
+
+	for(var i=0; i<numOfBalloons; i++){
+		$("#balloon-container").append("<div class='balloon'></div>");
+		if(i % 2 === 0){
+			$(".balloon").last().addClass("big");
+		}
+
+	}
+
+var audio = new Audio("pop.m4a");
+	$(".balloon").on("mouseenter",function() {
 		$(this).addClass("popped");
+		audio.play();
 		popCount = popCount + 1;
 		$("#pop-target").html(popCount);
 
 		//check did the user pop all the balloons?
-		if(popCount === 7){
+		if(popCount === numOfBalloons){
 			$("#bar").html("YOU WON!");
 
+
+
 		} 
-		else if  (popCount ===5){
+		/*else if  (popCount ===5){
 			alert("Keep Going!");
 		}
 		else{
 			console.log("Not done yet.")
-		}
+		}*/
+
+
 
 		
+
 	});
 
 

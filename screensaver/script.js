039785439953e8a8df3ab3adb7b685f7800f7361
Gabrielle Cantamessa <cantg831@newschool.edu>
2017-03-05 13:58:34 -0500

    screensaver edits

---
+++ screensaver/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+$("#ballcenter").on ("mouseenter", function(){
+	$(this).addClass("focused");
+});
+
+$("#ballcenter").on("mouseleave", function(){
+	$(this).removeClass("focused");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

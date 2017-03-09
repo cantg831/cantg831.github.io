6387cef585a2f06daa030ded9f348dd8b51fad44
Gabrielle Cantamessa <cantg831@newschool.edu>
2017-03-09 18:31:09 -0500

    edits to homepage

--- screensaver/script.js
+++ screensaver/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
-var messages = ["m1","m2","m3","m4","m5","m6","m7","m8","m9","m10","m11","m12","m13","m14","m15"];
+var messageCounter =0;
+
+setInterval(function){
+	$(".message").html(messages[messageCounter]);
+
+	messageCounter++;
+
+	if(messageCounter >= messages.length){
+		counter = 0;
+	 }
 
 
-for (var i=0; 1<YourCode.length;i++){
-	var random= Math.random() * messages.length;
-	random=Math.floor(random);
+	}, 5000);
+
+
+
+}
 
 	
 
--- structures/js/script.js
+++ structures/js/script.js
@@ -26,16 +26,30 @@ setInterval(function(){
 	}, 1000);
 
 
-$("#quote-target").html(singleQuote["quote"]);
-$("#source-target").html(singleQuote["source"]);
-$("#year-target").html(singleQuote["year"]);
+// $("#quote-target").html(singleQuote["quote"]);
+// $("#source-target").html(singleQuote["source"]);
+// $("#year-target").html(singleQuote["year"]);
 
+function updateAll(){
+ updateWord();
+ updateQuote();
 
+}
 
+function updateWord(){
+	$("#word-target").html(words[wordCounter]);
+	wordCounter++;
+	if(wordCounter === words.length){
+		wordCounter = 0;
+	}
+}
 
-
-
-
+function updateQuote(){
+	$("#quote-target").html(quotes[quoteCounter]["quote"]);
+	$("#source-target").html(quotes[quoteCounter]["source"]);
+	$("#year-target").html(quotes[quoteCounter]["source"]);
+		
+}
 
 
 

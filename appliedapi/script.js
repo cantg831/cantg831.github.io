268e12a5dc13fbd2b2100244fce2925f252e44f7
Gabrielle Cantamessa <cantg831@newschool.edu>
2017-04-05 12:44:01 -0400

    edits to applied api

--- appliedapi/script.js
+++ appliedapi/script.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
-  var url = "http://api.urbandictionary.com/v0/define?term=";
+  var url = "http://api.urbandictionary.com/v0/define?term=timbs";
+  // var url="http://api.urbandictionary.com/v0/define?term=dead ass";
+  // var url="http://api.urbandictionary.com/v0/define?term=good looks";
+  // var url="http://api.urbandictionary.com/v0/define?term=wavy";
+  // var url="http://api.urbandictionary.com/v0/define?term=brick";
+  // var url="http://api.urbandictionary.com/v0/define?term=chop cheese";
 
 $.getJSON(url,function(data) {
 
-	 console.log(data["define"]["word"]);
-	$("#word").html(data["definition"]["example"]);
+ console.log(data["list"][0]["definition"]);
+
+ $(".definition").html(data["list"][0]["definition"]);
+
+ $("button").on("click",function(){
+ 	$(data["list"][0]["definition"]);
+
+ });
+
+ // $("button").onClick(data["list"][0]["definition"]);
+
+
+
+
+
+ });
+
+	 
+});

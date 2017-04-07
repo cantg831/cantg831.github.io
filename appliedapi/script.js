023233fc90fb529e919823676005a92742260832
Gabrielle Cantamessa <cantg831@newschool.edu>
2017-04-06 21:38:11 -0400

    uodates to applied api and sign

--- appliedapi/script.js
+++ appliedapi/script.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
-  var url = "http://api.urbandictionary.com/v0/define?term=timbs";
-  // var url="http://api.urbandictionary.com/v0/define?term=dead ass";
-  // var url="http://api.urbandictionary.com/v0/define?term=good looks";
-  // var url="http://api.urbandictionary.com/v0/define?term=wavy";
-  // var url="http://api.urbandictionary.com/v0/define?term=brick";
-  // var url="http://api.urbandictionary.com/v0/define?term=chop cheese";
 
-$.getJSON(url,function(data) {
+  var urls=["http://api.urbandictionary.com/v0/define?term=timbs","http://api.urbandictionary.com/v0/define?term=dead ass","http://api.urbandictionary.com/v0/define?term=good looks","http://api.urbandictionary.com/v0/define?term=yurp","http://api.urbandictionary.com/v0/define?term=fugetaboutit"];
+  var url2="http://api.urbandictionary.com/v0/define?term=dead ass";
+  var url3="http://api.urbandictionary.com/v0/define?term=good looks";
+  var url4="http://api.urbandictionary.com/v0/define?term=yurp";
+  var url5="http://api.urbandictionary.com/v0/define?term=fugetaboutit";
+  var click=0;
 
- console.log(data["list"][0]["definition"]);
 
- $(".definition").html(data["list"][0]["definition"]);
+
+ 
 
  $("button").on("click",function(){
- 	$(data["list"][0]["definition"]);
+ 	click++
+ 	var number=$(this).attr("data-num");
+ 	// $(data["list"][0]["definition"]);
+ 	$.getJSON(urls[number],function(data) {
+ 		$(".definition").html(data["list"][0]["definition"]);
+
+ 		if (click===2) {
+
+ 			$(".gif"+ number).addClass("show")
+ 		}
+
+ 	console.log(data["list"][0]["definition"]);
 
  });
 
